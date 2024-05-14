1c73225f4943337a366ca51f8c8587e14b209706
ajwlukas <ajwlukas@gmail.com>
2024-05-14 14:15:34 +0900

    board 작성

--- src/store/slices/boardSlice.ts
+++ src/store/slices/boardSlice.ts
@@ -1,8 +1,61 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { IBoard } from "../../types";
 
-const initialState = {
+type TBoardState = {
+    modalActive : boolean;
+    boardArray: IBoard[];
+}
+
+const initialState : TBoardState = {
     modalActive : false,
-    boardArray:[]
+    boardArray:[
+        {
+            boardId:'board-0',
+            boardName:'첫 번 째 게시물',
+            lists:[
+                {
+                    listId:"list-0",
+                    listName:"list 0",
+                    tasks:[
+                        {
+
+                            taskId:"task-0",
+                            taskName:"task 0",
+                            taskDesc : "Description",
+                            taskOwner : "John"
+                        },
+                        {
+
+                            taskId:"task-1",
+                            taskName:"task 1",
+                            taskDesc : "Description",
+                            taskOwner : "Tom"
+                        }
+                    ]
+                },
+                {
+                    listId:"list-1",
+                    listName:"list 1",
+                    tasks:[
+                        {
+
+                            taskId:"task-3",
+                            taskName:"task 3",
+                            taskDesc : "Description",
+                            taskOwner : "John"
+                        },
+                        {
+
+                            taskId:"task-1",
+                            taskName:"task 2",
+                            taskDesc : "Description",
+                            taskOwner : "Tom"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 }
 
 const boardSlice  = createSlice({

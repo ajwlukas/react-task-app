c4ba2d798aba72d8bba4f4c93456b963403efe59
ajwlukas <ajwlukas@gmail.com>
2024-05-16 18:30:16 +0900

    action added to reducers

--- src/store/slices/boardSlice.ts
+++ src/store/slices/boardSlice.ts
@@ -1,9 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { IBoard } from "../../types";
 
 type TBoardState = {
     modalActive : boolean;
     boardArray: IBoard[];
+}
+
+type TAddBoardAction = {
+    board: IBoard;
 }
 
 const initialState : TBoardState = {
@@ -62,8 +66,11 @@ const boardSlice  = createSlice({
     name:'board',
     initialState,
     reducers:{
-
+        addBoard:(state, {payload}:PayloadAction<TAddBoardAction>)=>{
+            state.boardArray.push(payload.board)
+        }
     }
 })
 
+export const {addBoard} = boardSlice.actions;
 export const boardReducer = boardSlice.reducer;//sub reducer를 combine 해서 reducer를 만든다.

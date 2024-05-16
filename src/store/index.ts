379e141716a5b6231da3e1e8a23db357ce2424fe
ajwlukas <ajwlukas@gmail.com>
2024-05-16 18:29:46 +0900

    dispatch type fixed

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,7 +6,7 @@ const store = configureStore({
 })
 
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = ReturnType<typeof store.dispatch>
+export type AppDispatch = typeof store.dispatch
 
 
 export default store;

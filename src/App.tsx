34b51545960f27b62401d1817b6409880972edeb
ajwlukas <ajwlukas@gmail.com>
2024-05-16 12:57:06 +0900

    board List created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { appContainer, board, buttons } from "./App.css";
+import { useState } from "react";
+import { appContainer, board } from "./App.css";
+import BoardList from "./components/BoardList/BoardList";
 
 function App() {
+  const [activeBoardId, setActiveBoardId] = useState('board-0');
   return (
     <div className={appContainer}>
-      <div className={board}></div>
-
-      <div className={buttons}>
+      <div className={board}> </div>
+      <BoardList activeBoardId={activeBoardId} setActiveBoardId={setActiveBoardId}/>
+      <div>
         <button>이 게시판 삭제하기</button>
-
         <button></button>
       </div>
     </div>

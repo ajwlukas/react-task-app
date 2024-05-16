f2ed8d629068fdb797487cd7e8c16397f94ad345
ajwlukas <ajwlukas@gmail.com>
2024-05-16 13:47:06 +0900

    boardStyle created and applied

--- src/components/BoardList/BoardList.tsx
+++ src/components/BoardList/BoardList.tsx
@@ -2,6 +2,16 @@ import React, { FC, useState } from "react";
 import { useTypedSelector } from "../../hooks/redux";
 import SideForm from "./SideForm/SideForm";
 import { FiPlusCircle } from "react-icons/fi";
+import { IBoard } from "../../types";
+import {
+  addButton,
+  addSection,
+  boardItem,
+  boardItemActive,
+  container,
+  title,
+} from "./BoardList.css";
+import { clsx } from "clsx";
 
 type TBoardListProps = {
   activeBoardId: string;
@@ -16,19 +26,35 @@ const BoardList: FC<TBoardListProps> = ({
   const [isFormOpen, setIsFormOpen] = useState(false);
 
   return (
-    <div>
-      <div>게시판:</div>
-      {boardArray.map((board, index) => (
-        <div key={board.boardId}>
+    <div className={container}>
+      <div className={title}>게시판:</div>
+      {boardArray.map((board: IBoard, index: number) => (
+        <div
+          key={board.boardId}
+          onClick={()=>setActiveBoardId(boardArray[index].boardId)}
+          className={clsx(
+            {
+              [boardItemActive]:
+                boardArray.findIndex((b) => b.boardId === activeBoardId) ===
+                index,
+            },
+            {
+              [boardItem]:
+                boardArray.findIndex((b) => b.boardId === activeBoardId) !==
+                index,
+            }
+          )}
+        >
           <div>{board.boardName}</div>
         </div>
       ))}
-      <div>
+      <div className={addSection}>
         {
           isFormOpen ? (
             <SideForm />
           ) : (
             <FiPlusCircle
+              className={addButton}
               onClick={() => {
                 setIsFormOpen(!isFormOpen);
               }}

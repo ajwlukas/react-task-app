9c760f6b9db87103d0aba1e4d322d7ab240d5e84
ajwlukas <ajwlukas@gmail.com>
2024-05-16 18:51:26 +0900

    list container added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
-import { appContainer, board } from "./App.css";
+import { appContainer, board, buttons } from "./App.css";
 import BoardList from "./components/BoardList/BoardList";
+import ListsContainer from "./components/ListsContainer/ListsContainer";
+import { useTypedSelector } from "./hooks/redux";
 
 function App() {
   const [activeBoardId, setActiveBoardId] = useState("board-0");
+
+  const boards = useTypedSelector(state=>state.boards.boardArray);
+
+  const getActiveBoard = boards.filter(board => board.boardId === activeBoardId)[0];
+
+  const lists = getActiveBoard.lists;
+  
   return (
     <div className={appContainer}>
       <BoardList
@@ -11,10 +20,18 @@ function App() {
         setActiveBoardId={setActiveBoardId}
       />
 
-      <div className={board}> </div>
-      <div>
-        <button>이 게시판 삭제하기</button>
-        <button></button>
+      <div className={board}>
+        <ListsContainer lists = {lists} boardId={getActiveBoard.boardId}/>
+
+      </div>
+
+      <div className={buttons}>
+        <button>
+          이 게시판 삭제하기
+          </button>
+        <button>
+
+        </button>
       </div>
     </div>
   );

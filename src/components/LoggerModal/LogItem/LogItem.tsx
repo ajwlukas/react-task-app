4cc1daa8ce48db5d12ea8f06a3a61dccdfbf712f
ajwlukas <ajwlukas@gmail.com>
2024-05-19 15:15:51 +0900

    file name fixed

---
+++ src/components/LoggerModal/LogItem/LogItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LogItem = () => {
+  return (
+    <div>Log Item</div>
+  )
+}
+
+export default LogItem
--- src/components/LoggerModal/LogItem/LogeItem.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const LogeItem = () => {
-  return (
-    <div>LogeItem</div>
-  )
-}
-
-export default LogeItem

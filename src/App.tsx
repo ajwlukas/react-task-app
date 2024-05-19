8d6c081ec7d9236b7e7dddee3905188988967f54
ajwlukas <ajwlukas@gmail.com>
2024-05-19 15:16:02 +0900

    logger modal created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 //App.tsx
 import { useState } from "react";
-import { appContainer, board, buttons } from "./App.css";
+import { appContainer, board, buttons, deleteBoardButton, loggerButton } from "./App.css";
 import BoardList from "./components/BoardList/BoardList";
 import ListsContainer from "./components/ListsContainer/ListsContainer";
 import { useTypedSelector } from "./hooks/redux";
 import EditModal from "./components/EditModal/EditModal";
+import LoggerModal from "./components/LoggerModal/LoggerModal";
 
 function App() {
+  const [isLoggerOpen, setIsLoggerOpen] = useState(true);
   const [activeBoardId, setActiveBoardId] = useState("board-0");
 
   const modalActive =  useTypedSelector(state=>state.boards.modalActive);
@@ -16,10 +18,11 @@ function App() {
   const getActiveBoard = boards.filter(board => board.boardId === activeBoardId)[0];
 
   const lists = getActiveBoard.lists;
+
   
   return (
     <div className={appContainer}>
-
+      {isLoggerOpen? <LoggerModal setIsLoggerOpen={setIsLoggerOpen}/> : null}
       {modalActive ? <EditModal/> : null }
       
       <BoardList
@@ -33,11 +36,12 @@ function App() {
       </div>
 
       <div className={buttons}>
-        <button>
+        <button className={deleteBoardButton}>
           이 게시판 삭제하기
           </button>
-        <button>
-
+        <button onClick={()=>setIsLoggerOpen(!isLoggerOpen)}
+        className={loggerButton}>
+          {isLoggerOpen? "활동 목록 숨기기":"활동 목록 보이기"}
         </button>
       </div>
     </div>
